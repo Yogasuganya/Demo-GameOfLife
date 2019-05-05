5b6f980dbc08529496191cc489ca34edff8dd831
yoga <yogasugait@gmail.com>
2019-05-04 19:23:39 -0600

    Initial release commit for game of life.

---
+++ precache-manifest.09c42ba6c44c39e68c4fef15b0c96e3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ac9ced736084ac7fdd8",
+    "url": "/GameOfLife/static/js/runtime~main.4a239a3b.js"
+  },
+  {
+    "revision": "ee7c425f220a2dcbd730",
+    "url": "/GameOfLife/static/js/main.313d602a.chunk.js"
+  },
+  {
+    "revision": "351008de438e9288e220",
+    "url": "/GameOfLife/static/js/2.28e67316.chunk.js"
+  },
+  {
+    "revision": "ee7c425f220a2dcbd730",
+    "url": "/GameOfLife/static/css/main.e1378183.chunk.css"
+  },
+  {
+    "revision": "351008de438e9288e220",
+    "url": "/GameOfLife/static/css/2.a825ae56.chunk.css"
+  },
+  {
+    "revision": "c14674b0173e82431ccedd952b431381",
+    "url": "/GameOfLife/index.html"
+  }
+];
